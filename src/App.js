20aee1955f318ab76839ec60c168e53f80cb92f8
everlast <bilguunbj@gmail.com>
2022-09-05 09:56:06 +0800

    v2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,70 @@
-import "./App.css";
+import "./index.css";
+import Employee from "./components/Employee";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [role, setRole] = useState("dev");
+  const [employees, setEmployees] = useState([
+    {
+      name: "Bilguun",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/9989392/pexels-photo-9989392.jpeg",
+    },
+    {
+      name: "Bilguun",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/9989392/pexels-photo-9989392.jpeg",
+    },
+    {
+      name: "Bilguun",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/9989392/pexels-photo-9989392.jpeg",
+    },
+    {
+      name: "Bilguun",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/9989392/pexels-photo-9989392.jpeg",
+    },
+    {
+      name: "Bilguun",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/9989392/pexels-photo-9989392.jpeg",
+    },
+    {
+      name: "Bilguun",
+      role: "CEO",
+      img: "https://images.pexels.com/photos/9989392/pexels-photo-9989392.jpeg",
+    },
+  ]);
+  const showEmployees = true;
   return (
     <div className="App">
-      <header className="App-header">
-        <p>.</p>
-      </header>
+      {showEmployees ? (
+        <>
+          <input
+            type="text"
+            onChange={(e) => {
+              console.log(e.target.value);
+              setRole(e.target.value);
+            }}
+          />
+          <div className="flex flex-wrap justify-center">
+            {employees.map((employee) => {
+              return (
+                <Employee
+                  key={uuidv4()}
+                  name={employee.name}
+                  role={employee.role}
+                  img={employee.img}
+                />
+              );
+            })}
+          </div>
+        </>
+      ) : (
+        <p>Harj bolohgui</p>
+      )}
     </div>
   );
 }
